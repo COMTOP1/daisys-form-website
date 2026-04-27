afaf551b507ac97712736c89e55dc80aadcc562e
Liam Burnand <liam.burnand@couchbase.com>
2026-04-27 13:23:46 +0100

    Moving away from git

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
-import {execFileSync} from "node:child_process";
 
-const gitCommit =
-    process.env.DFW_GIT_HASH ??
-    execFileSync("/usr/bin/git", ["rev-parse", "HEAD"]).toString().trim();
+const gitCommit = process.env.DFW_GIT_HASH ?? "unknown";
 const version = process.env.DFW_VERSION_ARG ?? "v0.0.0";
 
 const nextConfig: NextConfig = {
